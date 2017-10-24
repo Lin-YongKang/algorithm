1f3c28d89b98058fc1cb0f02d57b8619395d66f5
linyongkang <18825046970@163.com>
2017-10-24 13:38:48 +0800

    增加js-docs

--- src/action/index.js
+++ src/action/index.js
@@ -1,5 +1,10 @@
 const model = require("$model");
-
+/**
+ * Set the shoe's color.
+ *
+ * @param {SHOE_COLORS} color - The shoe color. Must be an enumerated
+ * value of {@link SHOE_COLORS}.
+ */
 exports.timeOut = async second => {
     return await new Promise(resolve => {
         setTimeout(() => resolve(model), second * 1000);
