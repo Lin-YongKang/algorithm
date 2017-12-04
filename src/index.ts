95532d8c16bfb5b786681e3e834941626197cf32
linyongkang <18825046970@163.com>
2017-12-04 15:54:42 +0800

    使用ts-node运行项目

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@
 // // import selectTest from "./select/test";
 // // selectTest();
 
-import graphTest from "./graph/test";
-graphTest();
+// import graphTest from "graph/test";
+// graphTest();
+
+import test = require("test");
+console.log(111);
+console.log(test(1, 2));
--- src/utils/FrequencyCounter.ts
+++ src/utils/FrequencyCounter.ts
@@ -1,17 +1,17 @@
 import * as select from "../select";
-import * as readline from 'readline';
-import * as fs from 'fs';
-import * as path from 'path';
-import Stopwatch from './Stopwatch';
+import * as readline from "readline";
+import * as fs from "fs";
+import * as path from "path";
+import Stopwatch from "./Stopwatch";
 const FILE = path.resolve(__dirname, "../../data/leipzig1m.txt");
 type Param = typeof select.DisorderedTableExample | typeof select.OrderedTableExample;
 export default class FrequencyCounter {
     public static test(...Selects: Param[]) {
-        this.readFile(FILE).then((lines) => {
+        this.readFile(FILE).then(lines => {
             Selects.forEach(select => {
                 this.selection(select, lines);
-            })
-        })
+            });
+        });
     }
     public static selection(Select: Param, lines: string[][]) {
         let watch = new Stopwatch();
@@ -20,20 +20,19 @@ export default class FrequencyCounter {
         lines.forEach(words => {
             words.forEach(word => {
                 if (word === "") return;
-                (!target.contains(word)) ? (target.put(word, 1)) : (target.put(word, target.get(word) + 1))
-            })
+                !target.contains(word) ? target.put(word, 1) : target.put(word, target.get(word) + 1);
+            });
         });
         console.log(Select.name, "after put", watch.intervalTime());
         console.log(Select.name, "before max", watch.intervalTime());
         let max = "";
         if (target instanceof select.DisorderedTableExample) {
-
             target.put(max, 0);
             for (let word of target.keys()) {
                 if (target.get(word) > target.get(max)) max = word;
             }
         } else if (target instanceof select.OrderedTableExample) {
-            max = target.max()
+            max = target.max();
         }
         console.log(Select.name, "after max", watch.intervalTime());
         console.log(Select.name, max + " " + target.get(max));
@@ -46,6 +45,6 @@ export default class FrequencyCounter {
             let lines: string[][] = [];
             rl.on("line", (line: string) => lines.push(line.replace(/,|\.|!|\?/g, "").split(/\s/)));
             rl.on("close", () => resolve(lines));
-        })
+        });
     }
 }
